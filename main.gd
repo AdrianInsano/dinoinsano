cfe6218476a929518728712a709e4f3dea7eafae
adrian <adrianlaramar@gmail.com>
2024-05-27 09:30:00 -0600

    agregue mobs

--- main.gd
+++ main.gd
@@ -10,9 +10,16 @@ func _ready():
 func new_game():
 	$Insaneplayer.position = PlayerInitialPosition
 	$Floor.position.x = 0
+	$MobTimer.start()
 
 func game_over():
 	GHUD.update_highscore_label()
 
 func _process(delta):
 	$Floor.position.x = $Insaneplayer.position.x - 150
+
+@export var mob_scene: PackedScene
+
+
+func _on_mob_timer_timeout():
+	print('creando mob...')
