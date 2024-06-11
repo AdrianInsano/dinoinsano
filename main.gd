3d7f3bce6348febb8dc9f37d3736f0f1feedfd94
adrian <adrianlaramar@gmail.com>
2024-06-11 14:27:21 -0600

    acabe el juego

--- main.gd
+++ main.gd
@@ -1,7 +1,10 @@
 extends Node2D
 
+@export var mob_scene: PackedScene
+
 var screen_size : Vector2i
 var PlayerInitialPosition
+
 
 func _ready():
 	screen_size = get_window().size
@@ -9,6 +12,7 @@ func _ready():
 	GHUD.start_game_btn.connect(new_game)
 
 func new_game():
+	print('el jeshua')
 	$Insaneplayer.position = PlayerInitialPosition
 	$Floor.position.x = 0
 	$MobTimer.start()
@@ -21,13 +25,11 @@ func game_over():
 func _process(delta):
 	$Floor.position.x = $Insaneplayer.position.x - 150
 
-@export var mob_scene: PackedScene
 
 func _on_mob_timer_timeout():
 	if GHUD.mob_counter < 2:
 		var mob = mob_scene.instantiate()
 		mob.position.x = $Insaneplayer.position.x + 1500
-		mob.position.y = 610
+		mob.position.y = 640
 		add_child(mob)
 		mob.hit.connect(game_over)
-
